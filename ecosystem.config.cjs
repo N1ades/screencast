5bd3121d6dc8d58acdc5e8cd1ab0a5ebb16ce111
MaxMIs <27479366+MaxMIs@users.noreply.github.com>
2025-06-13 03:05:33 +0000

    add rtmp server

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -8,6 +8,18 @@ module.exports = {
     apps: [
         {
             cwd: __dirname,
+            name: "rtmp",
+            script: `${__dirname}/rtmp-server/nginx/sbin/nginx`,
+            args: `-c ${__dirname}/rtmp-server/nginx/conf/nginx.conf`,
+            exp_backoff_restart_delay: 100,
+            env,
+            out_file: `${__dirname}/rtmp-server/nginx/logs/nginx.log`,
+            error_file: `${__dirname}/rtmp-server/nginx/logs/nginx.log`,
+            merge_logs: true,
+            watch: false,
+        },
+        {
+            cwd: __dirname,
             name: "cast",
             script: "npm",
             // args: "upload/index.js",
